282bdda54dea6e02ea88c8271a56a2d0f9ebb6dd
Alex Nagy <therebeloper@gmail.com>
2020-12-14 18:28:27 +0200

    Initial Commit

---
+++ NavigationKitDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationKitDemo
+//
+//  Created by Alex Nagy on 14.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationKitDemo/NavigationKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationKitDemoApp.swift
+//  NavigationKitDemo
+//
+//  Created by Alex Nagy on 14.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct NavigationKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
